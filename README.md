50b9625816b67bf7735a12bb06517a89f632ac7b
Nina Nguyen <ninamailan90@gmail.com>
2019-10-28 08:59:48 -0700

    update circle detection of binary image

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # logo-recognition-from-video
-Logo detection and recognition from advertisement videos.
+Logo detection and recognition from advertisement videos using OpenCV and TensorFlow.
+
+
+
+# Process
+
+### 0) Obtain .mp4 advertisement videos
+<img src='img/youtube_video.png'>Pull videos from youtube.</img>
+
+
+### 1) Create .jpg frames from .mp4 video
+<img src='img/frame1906_original.jpg'>Extract frames.</img>
+<br>use frame_extractor.py
+
+### 2) Create binary image
+<img src='img/frame1906_binary.png'>Convert to binary to simplify segmentation.</img>
+<br>use label_segments in detect_circle.py
+
+
+### 3) Detect circles
+use parse_image in detect_cicle.py
--- detect_circle.py
+++ detect_circle.py
@@ -7,34 +7,35 @@ from skimage import filters, color
 from scipy import ndimage as ndi
 from itertools import chain
 import matplotlib.colors as mcolors
-
 import matplotlib.pyplot as plt
 
  
-def label_segments(filename,savename='/testing_segment/'):
+def label_segments(filename,savename='/testing_segment/',pwd=''):
     imgname = filename.split('/')[-1][:-4]
     image = io.imread(filename)
     gray_image = color.rgb2gray(image)
     thresh = filters.threshold_mean(gray_image)
     binary = gray_image > thresh
-    io.imsave(savename+imgname+'_original.png',binary*255)
-
-    label_arr, num_seg = ndi.label(np.invert(binary))
+    bw_img = pwd+savename+imgname+'_binary.jpg'
+    cv2.imwrite(bw_img,binary*255)
+    # parse_image(filename)
+    # parse_image(bw_img,grey=True)
+    label_arr, num_seg = ndi.label(binary*255)     ##invert np.invert(binary))  
     segments = np.arange(1,num_seg+1)
     return binary,np.array(label_arr),segments,image
 
-def plot_numbered_image(label_arr,savename='/testing_segment/',no_rotate=False,filepath=''):
+def plot_numbered_image(label_arr,savename='/testing_segment/',rotate=True,filepath=''):
     colors = np.array(list(chain(mcolors.TABLEAU_COLORS.values())))
-    # np.repeat(colors,2)                                         ### put in repeat for large sets
-    if no_rotate: pixarray=np.rot90(label_arr,3)
+    colors = np.repeat(colors,20000)                                         ### put in repeat for large sets
+    if rotate: pixarray=np.rot90(label_arr,3)
     else: pixarray=label_arr
     imax,jmax = pixarray.shape
     fig,ax=plt.subplots(ncols=1, nrows=1, figsize=(20,int(20*jmax/imax)))
     plt.xticks(np.arange(0,imax))
     plt.yticks(np.arange(0,jmax))
     np.random.shuffle(colors)
-    for i in range(0,imax,10):
-        for j in range(0,jmax,10):
+    for i in range(0,imax):
+        for j in range(0,jmax):
             val = pixarray[i][j]
             if val != 0:
                 ax.text(i,j,val,fontsize=20,color=colors[val])
@@ -46,16 +47,23 @@ def plot_numbered_image(label_arr,savename='/testing_segment/',no_rotate=False,f
     plt.close(fig)
     return
 
-def parse_image(imagepath):
+def parse_image(imagepath,grey=False):
 
     # binary_arr,label_arr, segments,image = label_segments(imagepath,'testing_segment/')
     # plot_numbered_image(label_arr,filepath=imagepath)
+    print("processing",imagepath,"....")
     image = cv2.imread(imagepath)
+    print(image.shape[0],image.shape[1])
+    # if grey:
+    #     image =cv2.merge([image,image])
     output = image.copy()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 10000)
-
+    # thresh = filters.threshold_mean(gray)
+    # binary = gray > thresh    
+    blurred = cv2.blur(gray,(7,7))
+    # circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 2, 50)
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT,20,blurred.shape[0]/64, param1=200, param2=10, minRadius=20, maxRadius=100)
+    #cv2.GaussianBlur( gray, gray, (9, 9), 2, 2 )
     # ensure at least some circles were found
     if circles is not None:
         # convert the (x, y) coordinates and radius of the circles to integers
@@ -65,22 +73,24 @@ def parse_image(imagepath):
         for (x, y, r) in circles:
             # draw the circle in the output image, then draw a rectangle
             # corresponding to the center of the circle
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.circle(output, (x, y), r, (255, 0, 0) , 4)
             cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
     
         # show the output image
         cv2.imshow("output", np.hstack([image, output]))
         cv2.waitKey(0)
-
+    cv2.destroyAllWindows()
+    return
 
 if __name__ == '__main__': 
-    path = "/home/nina/video-image-recognition/testing/"
+    path = "/home/nina/logo-recognition-from-video/testing/"
+    pwd =  "/home/nina/logo-recognition-from-video"
     images = os.popen("ls "+path).read().split('\n')[:-1]
-    firstimage = images[:2]
-    # for image in firstimage:
-    #     imagepath = path+image
-    #     print("processing ",imagepath,"....")
-    #     parse_image(imagepath)
-    testing = path+"circles.jpg"
-    print(testing)
-    parse_image(testing)
+    firstimage = images[5:6]
+    for image in firstimage:
+        imagepath = path+image
+        label_segments(imagepath,pwd=pwd)
+        # parse_image(imagepath)
+    # testing = path+"single_circle.jpg"
+    # print(testing)
+    # parse_image(testing)
